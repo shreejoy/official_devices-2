8d03836b9fdf1f89084c3ea537f75a9d13788e79
Mohan C M <mohanmanjappa@gmail.com>
2021-03-24 05:14:16 +0000

    fix api

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -10,8 +10,8 @@ git clone https://github.com/DotOS/website_api -b master api
 python3 generate_api.py
 
 cd api
-git add .
-CHANGED=$(git diff --numstat --cached | wc -l)
-[[ $CHANGED -eq 0 ]] && exit 0
-git commit -m "generate_api: $(date)"
-git push https://"${GUSER}":"${GPASSWORD}"@github.com/DotOS/website_api master 
+# git add .
+# CHANGED=$(git diff --numstat --cached | wc -l)
+# [[ $CHANGED -eq 0 ]] && exit 0
+# git commit -m "generate_api: $(date)"
+# git push https://"${GUSER}":"${GPASSWORD}"@github.com/DotOS/website_api master 
